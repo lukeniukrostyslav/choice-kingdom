3d2f01600935611e8709cd8e2cd9f645081cb6de
lukeniukrostyslav <lukeniukrostyslav@gmail.com>
2026-09-18 01:21:14 +0200

    Configure Compose screenshot testing plugin

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.chaquo.python") version "17.0.0" apply false
+    id("com.android.compose.screenshot") version "0.0.1-alpha16" apply false
 }
