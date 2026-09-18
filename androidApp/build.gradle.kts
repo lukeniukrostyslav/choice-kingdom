028224310065d98d3e17b471d65e60823cabd236
lukeniukrostyslav <lukeniukrostyslav@gmail.com>
2026-09-18 11:18:07 +0200

    Align Kotlin with screenshot validation API metadata

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("com.chaquo.python") version "17.0.0" apply false
     id("com.android.compose.screenshot") version "0.0.1-alpha16" apply false
 }
